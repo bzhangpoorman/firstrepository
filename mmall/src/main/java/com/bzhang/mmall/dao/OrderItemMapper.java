161aca0934cb63ae6830af96e32e2a15a96d1913
bzhangpoorman <349016792@qq.com>
2018-09-09 11:00:40 +0800

    project start

---
+++ mmall/src/main/java/com/bzhang/mmall/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.bzhang.mmall.dao;
+
+import com.bzhang.mmall.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
